b2376aba7ef407503624f48c2bf79a7634bf2680
Liam Cabral Teixeira <liamcabral88@gmail.com>
2020-11-16 23:59:30 -0300

    more fixes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 require('dotenv').config();
 
 require('./database/index');
 
-const cors = require('cors');
-
-app.use(cors());
 app.use(express.json());
 
-const routes = require('./router/routes');
+const routes = require('./router/routes.js');
 app.use(routes);
 
 let port = 3333 || process.env.PORT;
