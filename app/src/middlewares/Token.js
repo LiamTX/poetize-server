0e0b995ddcf309ab0b49acc8bb38b4caf935f641
Liam Cabral Teixeira <liamcabral88@gmail.com>
2020-11-27 05:33:52 -0300

    some fixes

--- app/src/middlewares/Token.js
+++ app/src/middlewares/Token.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = {
     generateToken(params = {}){
-        return jwt.sign(params, process.env.NODE_TOKEN_SECRET, {
+        return jwt.sign(params, process.env.TOKEN_SECRET, {
             expiresIn: 86400
         })
     },
@@ -10,7 +10,7 @@ module.exports = {
         const parts = t.split(' ');
         const [discard, token] = parts;
 
-        const decoded = jwt.verify(token, process.env.NODE_TOKEN_SECRET);
+        const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
 
         return decoded;
     }
