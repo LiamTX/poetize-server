4ca7a1db2a02b0657eb24775e495293c4d18de22
Liam Cabral Teixeira <liamcabral88@gmail.com>
2020-12-01 06:22:32 -0300

    fix //10000000

--- app/src/index.js
+++ app/src/index.js
@@ -5,16 +5,16 @@ const cors = require('cors');
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-    req.header("Access-Control-Allow-Origin", "*")
-    req.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
-    app.use(cors());
-    next();
-})
-
 require('dotenv').config();
 
 require('./database/index');
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
+    app.use(cors());
+    next();
+});
 
 const routes = require('./router/routes.js');
 app.use(routes);
