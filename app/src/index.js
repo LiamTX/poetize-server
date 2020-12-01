62f01fce31a52e6ba4ca594b995c9cba24a6836c
Liam Cabral Teixeira <liamcabral88@gmail.com>
2020-12-01 05:37:41 -0300

    cors /1000

--- app/src/index.js
+++ app/src/index.js
@@ -8,7 +8,7 @@ const corsOptions = {
 }
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 // app.use((req, res, next) => {
 //     res.header("Access-Control-Allow-Origin", "https://poetize.netlify.app")
