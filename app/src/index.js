12ec4eaed795a9e1c9c03a879a51d4e700f59268
Liam Cabral Teixeira <liamcabral88@gmail.com>
2020-12-01 06:33:05 -0300

    cors //3000

--- app/src/index.js
+++ app/src/index.js
@@ -5,12 +5,9 @@ const cors = require('cors');
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
-    app.use(cors());
-    next();
-});
+// app.use(cors());
+
+app.options('*', cors());
 
 require('dotenv').config();
 
