a11c4ed55b83f809eacc2391cf4b7bcb9681c883
Liam Cabral Teixeira <liamcabral88@gmail.com>
2020-12-01 04:41:29 -0300

    try to fix

--- app/src/index.js
+++ app/src/index.js
@@ -3,16 +3,16 @@ const app = express();
 
 const cors = require('cors');
 app.use(cors());
+app.use(express.json());
 
 require('dotenv').config();
 
 require('./database/index');
 
 const routes = require('./router/routes.js');
-app.use(express.json());
 app.use(routes);
 
-// console.log(process.env.DB_DATABASE)
+console.log(process.env.TOKEN_SECRET)
 
 let port = process.env.PORT || 3333;
 
