8fc65980fbd2ade04c872ec5ee151414f4bc842e
Liam Cabral Teixeira <liamcabral88@gmail.com>
2020-12-01 06:23:38 -0300

    reefix

--- app/src/index.js
+++ app/src/index.js
@@ -5,16 +5,16 @@ const cors = require('cors');
 
 app.use(express.json());
 
-require('dotenv').config();
-
-require('./database/index');
-
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
     app.use(cors());
     next();
 });
+
+require('dotenv').config();
+
+require('./database/index');
 
 const routes = require('./router/routes.js');
 app.use(routes);
