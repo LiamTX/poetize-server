a17b524f1c5a424f12b570f8edeff786c82058c8
Liam Cabral Teixeira <liamcabral88@gmail.com>
2020-12-01 04:45:36 -0300

    cors fix

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 
 const cors = require('cors');
-app.use(cors());
+app.use((req, res, next) => {
+    //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+    //Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 app.use(express.json());
 
 require('dotenv').config();
@@ -16,4 +23,4 @@ console.log(process.env.TOKEN_SECRET)
 
 let port = process.env.PORT || 3333;
 
-app.listen(port, () => {console.log('Started!')});
+app.listen(port, () => { console.log('Started!') });
