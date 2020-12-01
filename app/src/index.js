da7eb3cbc3d106ed7d0256130534fc17e9836c1c
Liam Cabral Teixeira <liamcabral88@gmail.com>
2020-12-01 05:47:14 -0300

    setHeader

--- app/src/index.js
+++ app/src/index.js
@@ -2,25 +2,15 @@ const express = require('express');
 const app = express();
 
 const cors = require('cors');
-// const corsOptions = {
-//     origin: 'https://poetize.netlify.app',
-//     optionsSuccessStatus: 200
-// }
 
 app.use(express.json());
-// app.use(cors());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
     app.use(cors());
     next();
 })
-
-// app.use((req, res, next) => {
-//     app.use(cors());
-//     next();
-// });
 
 require('dotenv').config();
 
